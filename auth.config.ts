afbf2179a0e9fd8572410df7b155608d2fcc426e
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-05-13 00:06:50 +0530

    middleware and auth intial setup

---
+++ auth.config.ts
@@ -0,0 +1,50 @@
+// import { NextResponse } from "next/server";
+import { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  providers: [],
+  callbacks: {
+    async authorized({ request, auth }) {
+      // Array of regex of paths to protect
+      const protectedPaths: RegExp[] = [
+        // /\/shipping-address/,
+        // /\/payment-method/,
+        // /\/place-order/,
+        // /\/profile/,
+        // /\/user\/(.*)/,
+        // /\/order\/(.*)/,
+        // /\/admin/,
+      ];
+
+      // Get pathname from the req url object
+      const { pathname } = request.nextUrl;
+
+      // check if user is not authenicated & accessing a protected route
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
+      // Check for cart session cookie
+      //   if (!request.cookies.get("sessionCartId")) {
+      //     // Generate new session cartId cookie
+      //     const sessionCartId = crypto.randomUUID();
+
+      //     // clone request headers
+      //     const newReqHeaders = new Headers(request.headers);
+
+      //     // create new response and add new headers
+      //     const response = NextResponse.next({
+      //       request: {
+      //         headers: newReqHeaders,
+      //       },
+      //     });
+
+      //     // set newly generated sessionCartId in the response cookies
+      //     response.cookies.set("sessionCartId", sessionCartId);
+      //     return response;
+      //   } else {
+      //     return true;
+      //   }
+
+      return true;
+    },
+  },
+} satisfies NextAuthConfig;
