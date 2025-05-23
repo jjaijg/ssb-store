0094fa88adfa847a6c33d271675b442f49613186
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-05-23 21:40:51 +0530

    user validation schema

---
+++ lib/validationSchema/user.schema.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+// Auth schemas - START
+
+// Schema for user registration
+export const signupFormSchema = z
+  .object({
+    name: z.string().min(3, { message: "Name must be atleast 3 characters" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" }),
+    confirmPassword: z.string().min(6, {
+      message: "Confirm Password must be at least 6 characters long",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+// Schema for user login
+export const signinFormSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+});
+
+// Auth schemas - END
+
+// user schemas - START
+// Schema for user profile update
+// Schema for updateing user profile
+export const updateProfileSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  email: z.string().min(3, "Email must be at least 3 characters"),
+});
+// user schemas - END
