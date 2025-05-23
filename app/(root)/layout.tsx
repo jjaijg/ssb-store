c04ec2864a57b6dead6de6eb0e60925a7e665ae0
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-05-23 21:38:48 +0530

    Root layout changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,15 @@
+import AppHeader from "@/components/shared/header";
 import React, { PropsWithChildren } from "react";
 
 type Props = {};
 
 const RootLayout = ({ children }: PropsWithChildren) => {
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-col h-screen">
+      <AppHeader />
+      <main className="flex-1 wrapper">{children}</main>
+    </div>
+  );
 };
 
 export default RootLayout;
