e1f4e2cae698b0b7fede8439de7642666268c05f
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-05-23 21:39:05 +0530

    Admin layout skeleton

---
+++ app/admin/layout.tsx
@@ -0,0 +1,27 @@
+import AppHeader from "@/components/shared/header";
+import React, { PropsWithChildren } from "react";
+import MainNav from "../../components/shared/main-nav";
+
+const links = [
+  { label: "Dashboard", href: "/admin/dashboard" },
+  { label: "Products", href: "/admin/Products" },
+  { label: "Orders", href: "/admin/orders" },
+  { label: "Users", href: "/admin/users" },
+];
+
+// This layout is used for the admin section of the application
+const AdminLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <AppHeader
+        hideBranding
+        hideSearch
+        hideCategory
+        renderMenu={() => <MainNav links={links} />}
+      />
+      <main className="flex-1 wrapper">{children}</main>
+    </div>
+  );
+};
+
+export default AdminLayout;
