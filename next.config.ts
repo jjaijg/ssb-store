1ce82b22c9e773e7061a7f23543e3ba0b37538e3
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-05-26 22:50:32 +0530

    updated config to include uploadthing host

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "b7oaky019a.ufs.sh",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
