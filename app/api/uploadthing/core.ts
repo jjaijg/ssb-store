e85ff38f19d9da469fc852b373229ef271b347ac
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-05-26 22:49:28 +0530

    uploadthing config

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,34 @@
+import { auth } from "@/auth";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    // Set permissions and file types for this FileRoute
+    .middleware(async () => {
+      // This code runs on your server before upload
+      const session = await auth();
+      if (!session) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: session?.user.id };
+    })
+    .onUploadComplete(async ({ metadata }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      // console.log("Upload complete for userId:", metadata.userId);
+
+      // console.log("file url", file.url);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
