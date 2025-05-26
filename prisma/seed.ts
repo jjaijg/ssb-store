e3354fc4f58ef0674900a8f784f633bed80c8a72
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-05-26 08:51:33 +0530

    updated seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,49 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const hashedPassword = await bcrypt.hash("admin123", 10);
+const categories = [
+  { name: "Grains", slug: "grains" },
+  { name: "Pulses", slug: "pulses" },
+  { name: "Soaps", slug: "soaps" },
+  { name: "Washing Liquids", slug: "washing-liquids" },
+  { name: "Toothpaste", slug: "toothpaste" },
+  { name: "Shampoos", slug: "shampoos" },
+  { name: "Body Wash", slug: "body-wash" },
+  { name: "Face Wash", slug: "face-wash" },
+  { name: "Spices", slug: "spices" },
+];
 
-  await prisma.user.create({
-    data: {
+const createAdminUser = async () => {
+  const hashedPassword = await bcrypt.hash("password", 10);
+  await prisma.user.upsert({
+    where: { email: "admin@ssb.in" },
+    update: {},
+    create: {
       email: "admin@ssb.in",
       name: "Admin User",
       role: "ADMIN",
       password: hashedPassword,
     },
   });
+};
 
-  console.log("Admin user created!");
+const createCategories = async () => {
+  for (const category of categories) {
+    await prisma.category.upsert({
+      where: { slug: category.slug },
+      update: {},
+      create: category,
+    });
+  }
+};
+
+async function main() {
+  // creating admin user
+  await createAdminUser();
+  // creating categories
+  await createCategories();
 }
 
 main()
